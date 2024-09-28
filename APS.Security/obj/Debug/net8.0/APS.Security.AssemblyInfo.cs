2ded0e2532079f30b85fdf874e90da4dd356d917
Marcelo Araya Briceño <maraya60675@ufide.ac.cr>
2024-09-28 09:49:07 -0600

    base de proyecto

--- APS.Security/obj/Debug/net8.0/APS.Security.AssemblyInfo.cs
+++ APS.Security/obj/Debug/net8.0/APS.Security.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APS.Security")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33155bff7b972889a6eb2405e1500eaa40b325d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d31f6e92d23bf88c5c4abe5b5acc9919ffd0d8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("APS.Security")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APS.Security")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
