f0c805b53a7bf5bdfda4eb1a99125433da388ca8
Wayner Jiménez <waynerjive@gmail.com>
2024-10-31 19:49:25 -0600

    Creacion de la vista de productos para el cliente junto con el carrito

---
+++ APS.Data/Models/Producto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APS.Data.Models
+{
+    public partial class Producto
+    {
+        [Key]
+        public int IdProducto { get; set; }
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public decimal Precio { get; set; }
+
+        public int Stock { get; set; }
+
+        public string RutaImagen { get; set; }
+
+        public Boolean activo { get; set; } = true;
+
+        public DateTime FechaRegistro { get; set; } = DateTime.Now;
+
+    }
+}
---
+++ APS.Web/Controllers/TiendaController.cs
@@ -0,0 +1,97 @@
+﻿using APS.Data.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APS.Web.Controllers
+{
+    public class TiendaController : Controller
+    {
+
+        private readonly ApdatadbContext _context;
+
+        public TiendaController(ApdatadbContext context)
+        {
+            _context = context;
+        }
+        // GET: TiendaController
+        public ActionResult Index()
+        {
+            var productos = _context.Producto.ToList();
+            return View(productos);
+        }
+
+        public ActionResult Carrito()
+        {
+            return View();
+        }
+
+        // GET: TiendaController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: TiendaController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: TiendaController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TiendaController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: TiendaController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: TiendaController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: TiendaController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
