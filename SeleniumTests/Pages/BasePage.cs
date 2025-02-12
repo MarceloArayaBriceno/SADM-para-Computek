156be2d4925679a4183a0f7b69b1208289a3e745
Wayner Jimenez <waynerjive@gmail.com>
2025-02-11 23:47:24 -0600

    Iniciacion del codigo para la implementacion de Selenium para pruebas automatizadas

---
+++ SeleniumTests/Pages/BasePage.cs
@@ -0,0 +1,47 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers; 
+using System;
+
+namespace SeleniumTests.Pages
+{
+    internal class BasePage
+    {
+        protected IWebDriver driver;
+        protected WebDriverWait wait;
+
+        public BasePage(IWebDriver driver)
+        {
+            this.driver = driver; // Corregido el error de asignación
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10)); // Corregido el tipo
+        }
+
+        protected IWebElement WaitAndFindElement(By by)
+        {
+            return wait.Until(ExpectedConditions.ElementIsVisible(by)); // Corregido el uso de ExpectedConditions
+        }
+
+        protected void WaitAndClick(By by)
+        {
+            WaitAndFindElement(by).Click();
+        }
+
+        protected void WaitAndSendKeys(By by, string text)
+        {
+            WaitAndFindElement(by).SendKeys(text);
+        }
+
+        protected bool IsElementPresent(By by)
+        {
+            try
+            {
+                WaitAndFindElement(by);
+                return true;
+            }
+            catch (WebDriverTimeoutException)
+            {
+                return false;
+            }
+        }
+    }
+}
